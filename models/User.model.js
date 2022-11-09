0f83155e78643d54f82e5449c6c5abf25eedb80c
MoniIgnacio <moniignacio02@gmail.com>
2022-11-09 19:47:29 +0400

    add branch dev

--- models/User.model.js
+++ models/User.model.js
@@ -17,7 +17,7 @@ const userSchema = new Schema(
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // timestamps: true
   }
 );
 

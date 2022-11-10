aac104336e7b5d7980aa8f6166a02ce357b17fbf
MoniIgnacio <moniignacio02@gmail.com>
2022-11-11 00:39:08 +0400

    Add routes User delete, patch & edit with middleware V-1.1

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,64 +2,61 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const isAuthenticated = require("../middlewares/auth.middlewares");
 
-  // GET '/user/:userId' => vista especifica de User
-  router.get("/:userId", isAuthenticated, async (req, res, next) => {
-    const { userId } = req.params;
-    try {
-      const response = await User.findById(userId);
-      res.status(201).json(response);
-    } catch (error) {
-      next(error);
+// GET '/user/:userId' => vista especifica de User
+router.get("/:userId", isAuthenticated, async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const response = await User.findById(userId);
+    res.status(201).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE '/user/:userId'=> delete especific user
+router.delete("/:userId", isAuthenticated, async (req, res, next) => {
+  const { userId } = req.params;
+  let userRole = req.payload.role;
+  try {
+    let userIdBd = await User.findById(userId);
+
+    let userIdStr = userIdBd._id.toString();
+    if (userRole === "admin" || req.payload._id === userIdStr) {
+      await User.findByIdAndDelete(userId);
+      res.status(200).json("User borrado success");
+    } else {
+      res.status(401).json("Needs a validated user");
     }
-  });
-  
-  // DELETE '/user/:userId'=> delete especific user
-  router.delete("/:userId", isAuthenticated, async (req, res, next) => {
-    const { userId } = req.params;
-    let userRole = req.payload.role;
-    try {
-      let userIdBd = await User.findById(userId);
-  
-      let userIdStr = userIdBd._id.toString();
-      if (userRole === "admin" || req.payload._id === userIdStr) {
-        await User.findByIdAndDelete(userId);
-        res.status(200).json("User borrado success");
-      } else {
-        res.status(401).json("Needs a validated user");
-      }
-    } catch (error) {
-      next(error);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PATCH '/user/:userId' => edit especific user
+router.patch("/:userId", isAuthenticated, async (req, res, next) => {
+  let userRole = req.payload.role;
+  const { userId } = req.params;
+  const { email, password, password2, username, phoneNumber, role } = req.body;
+
+  const userUpdate = {
+    username,
+    role,
+    phoneNumber,
+  };
+  try {
+    let userIdBd = await User.findById(userId);
+
+    let userIdStr = userIdBd._id.toString();
+
+    if (userRole === "admin" || req.payload._id === userIdStr) {
+      await User.findByIdAndUpdate(userId, userUpdate);
+      res.status(201).json("User actualizado success");
+    } else {
+      res.status(401).json("Needs validated user");
     }
-  });
-  
-  // PATCH '/user/:userId' => edit especific user
-  router.patch("/:userId", isAuthenticated, async (req, res, next) => {
-      let userRole = req.payload.role;
-      const { userId } = req.params;
-      const { email, password, password2, username, phoneNumber, role } = req.body;
-      
-      const userUpdate = {
-        username,
-        role,
-        phoneNumber
-      };
-      try {
-        let userIdBd = await User.findById(userId);
-  
-        let userIdStr = userIdBd._id.toString();
-        console.log(userIdStr)
-  
-        if (userRole === "admin" || req.payload._id === userIdStr) {
-          await User.findByIdAndUpdate(userId, userUpdate);
-          res.status(201).json("User actualizado success");
-        } else {
-          res.status(401).json("Needs validated user");
-        }
-      } catch (error) {
-        next(error);
-      }
-    }
-  );
-    
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;

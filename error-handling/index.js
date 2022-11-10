b244c9ef883eec2f9e4178a1e32e07da8f611427
MoniIgnacio <moniignacio02@gmail.com>
2022-11-10 16:30:46 +0400

    add auth-middleware

--- error-handling/index.js
+++ error-handling/index.js
@@ -9,6 +9,11 @@ module.exports = (app) => {
     // always logs the error
     console.error("ERROR", req.method, req.path, err);
 
+    if (err.name === "UnauthorizedError") {
+      res.status(401).json({errorMessage: "Token no valido o no hay token"})
+      return;
+    }
+
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
       res
---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,32 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+const isAuthenticated = jwt({
+  secret: process.env.TOKEN_SECRET,
+  algorithms: ["HS256"],
+  requestProperty: "payload",
+  getToken: (req) => {
+    console.log(req.headers)
+    
+    if (req.headers === undefined || req.headers.authorization === undefined) {
+      console.log("No hay Token en el sistema")
+      return null;
+    }
+
+    // si el token existe, extraelo del string y retornalo de la funcion
+    const tokenArr = req.headers.authorization.split(" ")
+    const tokenType = tokenArr[0]
+    const token = tokenArr[1]
+
+    if (tokenType !== "Bearer") {
+      console.log("Tipo de token incorrecto")
+      return null;
+    }
+
+    console.log("Token entregado")
+    return token
+
+  }
+})
+
+
+module.exports = isAuthenticated

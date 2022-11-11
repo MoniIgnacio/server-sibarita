6e8e48efaa4214f604669daea65b516e7ac6f2e1
Jelle <jmanzanohaagsma@gmail.com>
2022-11-11 10:31:04 +0100

    Minor changes to user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,9 +5,17 @@ const isAuthenticated = require("../middlewares/auth.middlewares");
 // GET '/user/:userId' => vista especifica de User
 router.get("/:userId", isAuthenticated, async (req, res, next) => {
   const { userId } = req.params;
+  let userRole = req.payload.role;
   try {
-    const response = await User.findById(userId);
-    res.status(201).json(response);
+    let userIdBd = await User.findById(userId);
+    let userIdStr = userIdBd._id.toString();
+
+    if (userRole === "admin" || req.payload._id === userIdStr) {
+      const response = await User.findById(userId);
+      res.status(201).json(response);
+    } else {
+      res.status(401).json("Needs a validated user");
+    }
   } catch (error) {
     next(error);
   }
@@ -19,8 +27,8 @@ router.delete("/:userId", isAuthenticated, async (req, res, next) => {
   let userRole = req.payload.role;
   try {
     let userIdBd = await User.findById(userId);
-
     let userIdStr = userIdBd._id.toString();
+
     if (userRole === "admin" || req.payload._id === userIdStr) {
       await User.findByIdAndDelete(userId);
       res.status(200).json("User borrado success");
@@ -33,19 +41,19 @@ router.delete("/:userId", isAuthenticated, async (req, res, next) => {
 });
 
 // PATCH '/user/:userId' => edit especific user
+//!Probar a añadir la edición de la password más adelante
 router.patch("/:userId", isAuthenticated, async (req, res, next) => {
   let userRole = req.payload.role;
   const { userId } = req.params;
-  const { email, password, password2, username, phoneNumber, role } = req.body;
+  const { email, password, password2, username, phoneNumber} = req.body;
 
   const userUpdate = {
     username,
-    role,
     phoneNumber,
+    email,
   };
   try {
     let userIdBd = await User.findById(userId);
-
     let userIdStr = userIdBd._id.toString();
 
     if (userRole === "admin" || req.payload._id === userIdStr) {
